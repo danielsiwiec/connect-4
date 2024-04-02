09aaf80175e00c66fa63d8cd1bf6ba099eb8ffc5
Dan Siwiec <daniel.siwiec@gmail.com>
2024-04-01 19:40:15 -0700

    Print current player

--- game/main.py
+++ game/main.py
@@ -5,13 +5,15 @@ from game.game_rules import BOARD_DIMENSIONS, play_game
 
 def input_feeder():
     pattern = re.compile(f"^[0-{BOARD_DIMENSIONS - 1}]$")
+    current_player = 'A'
     while True:
-        print(f'Give input [0-{BOARD_DIMENSIONS - 1}]')
+        print(f'Player {current_player}: Give input [0-{BOARD_DIMENSIONS - 1}]')
         txt = input()
         if pattern.match(txt):
             yield int(txt)
         else:
             print(f'Must be a number between 0 and {BOARD_DIMENSIONS - 1}')
+        current_player = 'A' if current_player == 'B' else 'B'
 
 
 if __name__ == "__main__":
